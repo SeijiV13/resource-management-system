a9b93c1f527d95f44b3f86d3d2ef883d4e0ade7b
Seiji Villafranca <villafrancasra@gmail.com>
2022-06-01 13:12:27 +0800

    added firbase config on env and adjusted budget

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true, 
+  firebase: {
+    apiKey: "AIzaSyBFOhYDidSvJpxGjVDYMhRgkFy3qnWJAc4",
+    authDomain: "resource-management-581ab.firebaseapp.com",
+    projectId: "resource-management-581ab",
+    storageBucket: "resource-management-581ab.appspot.com",
+    messagingSenderId: "599020348129",
+    appId: "1:599020348129:web:6c75ec93d63953d1eacda0",
+    measurementId: "G-FX7628HMVE",
+    databaseURL: "https://resource-management-581ab-default-rtdb.asia-southeast1.firebasedatabase.app"
+  }
 };
